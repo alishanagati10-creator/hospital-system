970b5e2d234c5428e55bac275b3dde8b244374ec
alishanagati10-creator <alishanagati10@gmail.com>
2026-04-23 11:52:03 +0530

    Delete coding-platform directory

--- coding-platform/src/main/java/com/example/codingplatform/repository/ProblemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codingplatform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.codingplatform.model.Problem;
-
-public interface ProblemRepository extends JpaRepository<Problem, Long> {
-}
--- coding-platform/src/main/java/com/example/codingplatform/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.codingplatform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.codingplatform.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
